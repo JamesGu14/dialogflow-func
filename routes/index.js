fbadce0f6c0fb45d4cbae03fa552aa923fc13def
James Gu <james_gu14@hotmail.com>
2018-03-06 14:03:48 +0800

    test

--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,9 @@ var router = express.Router();
 router.post('/', function(req, res, next) {
 
   let city = req.body.result.parameters.city
-  let salary = req.body.result.parameters.salary.replace('$', '')
+  let salary = req.body.result.parameters.salary
 
-  let tax = salary / 10
-
-  let response = `Hi, by living in ${city}, you need to pay $${tax} personal income tax`
+  let response = `Hi, by living in ${city}, you need to pay $${salary} personal income tax`
 
   res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
   res.send(JSON.stringify({ "speech": response, "displayText": response }));
