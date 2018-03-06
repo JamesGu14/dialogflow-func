b8b3b1b23cf0a2d95dae8a3ac84a7450310ce1b5
James Gu <james_gu14@hotmail.com>
2018-03-06 14:44:43 +0800

    test

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,12 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/', function(req, res, next) {
+  res.render('index', {
+    title: 'Express'
+  })
+})
+
 /* GET home page. */
 router.post('/', function(req, res, next) {
 
