8b2f4510a4bde9abac4470c230d409a3c97f6e33
James Gu <james_gu14@hotmail.com>
2018-03-06 13:58:47 +0800

    u

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,11 @@ var router = express.Router();
 /* GET home page. */
 router.post('/', function(req, res, next) {
 
-  let response = "This is a sample response from your webhook!" //Default response from the webhook to show it's working
+  let city = req.body.result.parameters.city
+  let currency = req.body.result.parameters.currency
+
+
+  let response = `Hi, by living in ${city}, you need to pay $${currency / 10} personal income tax`
 
   res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
   res.send(JSON.stringify({ "speech": response, "displayText": response }));
